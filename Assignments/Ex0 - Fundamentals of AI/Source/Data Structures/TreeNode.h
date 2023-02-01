9c360d21233b5e36dea7b33d10f9fe4577f72ece
OctavioAnino <oaninobarber@ufl.edu>
2023-01-31 22:12:15 -0500

    finished implementing treenode. build in VS and test

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <deque>
+
 
 // TreeNode class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
@@ -6,15 +8,18 @@ namespace ufl_cap4053 { namespace fundamentals {
 	class TreeNode {
 	private:
 		T *element_;
-		std::vector *children_;
+		
 
 	public:
+		std::vector *children_;
+
 		TreeNode<T>() {
 			element_ = new T();
 			children_ = new std::vector<TreeNode<T>*>();
 		}
 		TreeNode<T>(T element) {
 			element_ = element;
+			children_ = new std::vector<TreeNode<T>*>();
 		}
 		const T& getData() {
 			return *element;
@@ -34,6 +39,32 @@ namespace ufl_cap4053 { namespace fundamentals {
 		void removeChild(size_t index)
 		{
 			children_->erase((children->begin() + index));
+		}
+		const void breadthFirstTraverse(void (*datafunction)(const T)) {
+			std::deque<TreeNode<T>*> *queue = new std::deque<TreeNode<T>*>();
+
+			queue->push_back(this);
+			
+			while(!queue->empty()) {
+				TreeNode *curr = queue->front();
+				queue->pop_front();
+				datafunction(*(curr->element));
+				for(TreeNode<T> *node: curr->children) {
+					queue->push_nack(node);
+				}
+			}
+		}
+		const void preOrderTraverse(void (*datafunction)(const T)) {
+			datafunction(*(this->element));
+
+			for(TreeNode<T> *node; this->children) 
+				node.preOrderTraverse(datafunction);
+		}
+		const void postOrderTraverse(void (*datafunction)(const T)) {
+			for(TreeNode<T> *node; this->children) 
+				node.preOrderTraverse(datafunction);
+			
+			datafunction(*(this->element));
 		}
 	};
 }}  // namespace ufl_cap4053::fundamentals
